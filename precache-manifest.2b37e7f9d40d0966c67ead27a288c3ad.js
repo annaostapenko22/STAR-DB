7a0b8788f3b8def86086bedff4bdd8c533148597
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-01-10 13:43:49 +0100

    Updates

---
+++ precache-manifest.2b37e7f9d40d0966c67ead27a288c3ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d115950bc4fb30c5a12ea3976e0cd472",
+    "url": "/STAR-DB/./index.html"
+  },
+  {
+    "revision": "0b6d66da4d0a1b4902bf",
+    "url": "/STAR-DB/./static/css/main.ba31df15.chunk.css"
+  },
+  {
+    "revision": "e5aa451bd5064e582229",
+    "url": "/STAR-DB/./static/js/2.0f4ed810.chunk.js"
+  },
+  {
+    "revision": "3fe0340a7705f205d99afc40cc4a6a53",
+    "url": "/STAR-DB/./static/js/2.0f4ed810.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b6d66da4d0a1b4902bf",
+    "url": "/STAR-DB/./static/js/main.f1dc01e1.chunk.js"
+  },
+  {
+    "revision": "72bfefd4f4b7ffe15810",
+    "url": "/STAR-DB/./static/js/runtime-main.d39a604b.js"
+  },
+  {
+    "revision": "79434f585a85ce38d82bac38823fe049",
+    "url": "/STAR-DB/./static/media/boom.79434f58.svg"
+  }
+]);
